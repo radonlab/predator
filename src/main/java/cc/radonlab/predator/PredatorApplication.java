b86eb831560a864fc6a8556b62fdc7d404ad1024
skylerlee <skyler.ac.lee@gmail.com>
2017-11-05 04:36:47 +0800

    Add comment

--- src/main/java/cc/radonlab/predator/PredatorApplication.java
+++ src/main/java/cc/radonlab/predator/PredatorApplication.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
 package cc.radonlab.predator;
 
 import org.springframework.boot.SpringApplication;
