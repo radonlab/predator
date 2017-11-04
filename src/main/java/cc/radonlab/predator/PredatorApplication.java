10185d044016a0f74ba16d9197749ed04a63f8ad
skylerlee <skyler.ac.lee@gmail.com>
2017-11-05 01:46:46 +0800

    Add web application

---
+++ src/main/java/cc/radonlab/predator/PredatorApplication.java
@@ -0,0 +1,11 @@
+package cc.radonlab.predator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PredatorApplication.class, args);
+    }
+}
