c56bcdeac826f2e7383f204986670b48afacb697
skylerlee <skyler.ac.lee@gmail.com>
2017-11-27 20:25:16 +0800

    Add STT service

---
+++ predator-api/src/main/java/cc/radonlab/predator/api/stt/SttService.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package cc.radonlab.predator.api.stt;
+
+public interface SttService {
+}
