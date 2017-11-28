acbe72b029bf2d5fa111d001822ac202ddfb95b3
skylerlee <skyler.ac.lee@gmail.com>
2017-11-28 20:14:41 +0800

    Update service

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/AsrServiceImpl.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/AsrServiceImpl.java
@@ -8,12 +8,29 @@ package cc.radonlab.predator.api.asr.service.impl;
 
 import cc.radonlab.predator.api.asr.domain.TextResult;
 import cc.radonlab.predator.api.asr.service.AsrService;
+import com.alibaba.idst.nls.NlsClient;
+import com.alibaba.idst.nls.protocol.NlsRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AsrServiceImpl implements AsrService {
+    @Value("${site.asr.acKeyId}")
+    private String acKeyId;
+
+    @Value("${site.asr.acKeySecret}")
+    private String acKeySecret;
+
+    private NlsClient client;
 
     public AsrServiceImpl() {
+        client = new NlsClient();
+        client.init();
+    }
+
+    public void sendRequest() {
+        NlsRequest request = new NlsRequest();
+        request.authorize(this.acKeyId, this.acKeySecret);
     }
 
     @Override
