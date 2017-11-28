078884ac2c277ddc3674cb52e1c58fdd70112a02
skylerlee <skyler.ac.lee@gmail.com>
2017-11-28 13:43:10 +0800

    Remove iflytek sdk

--- predator-api/src/main/java/cc/radonlab/predator/api/stt/service/impl/MscSttServiceImpl.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/stt/service/impl/MscSttServiceImpl.java
@@ -8,28 +8,12 @@ package cc.radonlab.predator.api.stt.service.impl;
 
 import cc.radonlab.predator.api.stt.domain.TextResult;
 import cc.radonlab.predator.api.stt.service.SttService;
-import com.iflytek.cloud.speech.SpeechConstant;
-import com.iflytek.cloud.speech.SpeechRecognizer;
-import com.iflytek.cloud.speech.SpeechUtility;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MscSttServiceImpl implements SttService {
-    private static final String STT_APP_ID = "";
-
-    private SpeechRecognizer recognizer;
 
     public MscSttServiceImpl() {
-        SpeechUtility.createUtility(SpeechConstant.APPID + "=" + STT_APP_ID);
-        recognizer = initRecognizer();
-    }
-
-    private SpeechRecognizer initRecognizer() {
-        SpeechRecognizer recognizer = SpeechRecognizer.createRecognizer();
-        recognizer.setParameter(SpeechConstant.LANGUAGE, "zh_cn"); // "zh_cn"/"en_us"
-        recognizer.setParameter(SpeechConstant.ACCENT, "mandarin");
-        recognizer.setParameter(SpeechConstant.AUDIO_SOURCE, "-1");
-        return recognizer;
     }
 
     @Override
