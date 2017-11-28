042a3fea6929e8248a70a58fb43ff05af90c24f7
skylerlee <skyler.ac.lee@gmail.com>
2017-11-28 20:32:42 +0800

    Update asr service

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/AsrServiceImpl.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/AsrServiceImpl.java
@@ -9,12 +9,15 @@ package cc.radonlab.predator.api.asr.service.impl;
 import cc.radonlab.predator.api.asr.domain.TextResult;
 import cc.radonlab.predator.api.asr.service.AsrService;
 import com.alibaba.idst.nls.NlsClient;
+import com.alibaba.idst.nls.NlsFuture;
+import com.alibaba.idst.nls.event.NlsEvent;
+import com.alibaba.idst.nls.event.NlsListener;
 import com.alibaba.idst.nls.protocol.NlsRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AsrServiceImpl implements AsrService {
+public class AsrServiceImpl implements AsrService, NlsListener {
     @Value("${site.asr.acKeyId}")
     private String acKeyId;
 
@@ -28,9 +31,16 @@ public class AsrServiceImpl implements AsrService {
         client.init();
     }
 
-    public void sendRequest() {
+    private NlsRequest createRequest() {
         NlsRequest request = new NlsRequest();
         request.authorize(this.acKeyId, this.acKeySecret);
+        return request;
+    }
+
+    public void sendQuery() throws Exception {
+        NlsRequest request = createRequest();
+        NlsFuture future = client.createNlsFuture(request, this);
+        future.await(1000 * 10);
     }
 
     @Override
@@ -38,5 +48,17 @@ public class AsrServiceImpl implements AsrService {
         TextResult result = new TextResult();
         result.setResult("hello world");
         return result;
+    }
+
+    @Override
+    public void onMessageReceived(NlsEvent nlsEvent) {
+    }
+
+    @Override
+    public void onOperationFailed(NlsEvent nlsEvent) {
+    }
+
+    @Override
+    public void onChannelClosed(NlsEvent nlsEvent) {
     }
 }
