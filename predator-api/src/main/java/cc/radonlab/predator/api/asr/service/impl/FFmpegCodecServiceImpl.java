4b44ade62107abb09381dccc5be8389438204cb2
skylerlee <skyler.ac.lee@gmail.com>
2017-11-29 20:00:44 +0800

    Add logger

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/FFmpegCodecServiceImpl.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/FFmpegCodecServiceImpl.java
@@ -7,12 +7,16 @@
 package cc.radonlab.predator.api.asr.service.impl;
 
 import cc.radonlab.predator.api.asr.service.CodecService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
 @Service
 public class FFmpegCodecServiceImpl implements CodecService {
+    private static Logger logger = LoggerFactory.getLogger(FFmpegCodecServiceImpl.class);
+
     public FFmpegCodecServiceImpl() {
     }
 
