3f4efbe53c639b107852d837bf4dc3ea81176a50
skylerlee <skyler.ac.lee@gmail.com>
2017-11-29 22:22:01 +0800

    Add temp file path

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/FFmpegCodecServiceImpl.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/service/impl/FFmpegCodecServiceImpl.java
@@ -9,15 +9,35 @@ package cc.radonlab.predator.api.asr.service.impl;
 import cc.radonlab.predator.api.asr.service.CodecService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.access.BootstrapException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 @Service
 public class FFmpegCodecServiceImpl implements CodecService {
     private static Logger logger = LoggerFactory.getLogger(FFmpegCodecServiceImpl.class);
 
-    public FFmpegCodecServiceImpl() {
+    private static String TMP_DIR = System.getProperty("java.io.tmpdir");
+    private static File PIPES_DIR = new File(TMP_DIR, "pdt-pipes");
+
+    private ConfigurableApplicationContext ctx;
+    private File pipe;
+
+    @Autowired
+    public FFmpegCodecServiceImpl(ConfigurableApplicationContext ctx) {
+        this.ctx = ctx;
+        this.pipe = initPipe();
+    }
+
+    public File getPipe() {
+        return pipe;
     }
 
     @Override
