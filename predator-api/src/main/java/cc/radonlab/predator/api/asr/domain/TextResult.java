019dfa37e412d2aeb4d0f6700384efbef926287d
skylerlee <skyler.ac.lee@gmail.com>
2017-12-01 00:16:34 +0800

    Add comment

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/domain/TextResult.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/domain/TextResult.java
@@ -12,6 +12,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 
+// Object format
+// sn: {number}  - sentence
+// ls: {boolean} - last sentence
+// bg: {number}  - begin
+// ed: {number}  - end
+// ws: {Array}   - words
+//   bg: {number} - begin
+//   cw: {Array}  - chinese word
+//     w : {string} - word
+//     sc: {number} - score
+
 public class TextResult {
     private static ObjectMapper parser = new ObjectMapper();
 
