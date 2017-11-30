6059586b646b39b69c68d94d0bfca4204ccb3962
skylerlee <skyler.ac.lee@gmail.com>
2017-11-30 13:20:23 +0800

    Enable async support in app

--- predator-main/src/main/java/cc/radonlab/predator/PredatorApplication.java
+++ predator-main/src/main/java/cc/radonlab/predator/PredatorApplication.java
@@ -8,8 +8,10 @@ package cc.radonlab.predator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PredatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(PredatorApplication.class, args);
