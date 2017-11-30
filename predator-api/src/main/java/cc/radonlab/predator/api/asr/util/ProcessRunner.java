6e5e8b427cafb666acc94e09df4ab31fa54ac5ad
skylerlee <skyler.ac.lee@gmail.com>
2017-12-01 02:28:37 +0800

    Add process runner

---
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/util/ProcessRunner.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package cc.radonlab.predator.api.asr.util;
+
+public class ProcessRunner<T> {
+    public ProcessRunner() {
+    }
+}
