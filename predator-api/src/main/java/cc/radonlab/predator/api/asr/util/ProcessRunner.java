32baa8986a058ffff98bf186fa713ffc0e5cf294
skylerlee <skyler.ac.lee@gmail.com>
2017-12-01 12:58:08 +0800

    Update process runner

--- predator-api/src/main/java/cc/radonlab/predator/api/asr/util/ProcessRunner.java
+++ predator-api/src/main/java/cc/radonlab/predator/api/asr/util/ProcessRunner.java
@@ -6,10 +6,24 @@
 
 package cc.radonlab.predator.api.asr.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProcessRunner implements Runnable {
+    private static Logger logger = LoggerFactory.getLogger(ProcessRunner.class);
+
+    public static ProcessRunner bind(Process process) {
+        return new ProcessRunner(process);
+    }
+
     private Process process;
 
     private ProcessRunner(Process process) {
         this.process = process;
+    }
+
+    @Override
+    public void run() {
+        logger.info("from worker");
     }
 }
